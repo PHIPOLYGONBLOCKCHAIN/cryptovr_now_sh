8685dd72d6b8f4a7dcbdb2c56b412c6ca2a75a68
Green_Heart: Russian Cybernetics! In the name of I. V. STALIN.* QUASZ::::ZSIMODO :Green_Heart <openwoord@mail.ru>
2021-06-06 05:51:00 +0000

    Update src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ function Index() {
       <p>
         <em><b>Project status:</b></em>
         
-        <span class={project-status}><b>The <em><b>Platform</b></em> development is just starting (since <i>June 6, 2021</i>).</b></span>
+        <span class={projectStatus}><b>The <em><b>Platform</b></em> development is just starting (since <i>June 6, 2021</i>).</b></span>
       </p>
 
 
